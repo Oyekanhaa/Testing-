64a36f2270fa80bf53b2dfce1293396834fd40c0
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-01-06 09:46:59 +0530

    Update owner.py

--- SACHIN_MUSIC/plugins/bot/owner.py
+++ SACHIN_MUSIC/plugins/bot/owner.py
@@ -10,16 +10,16 @@ start_txt = """
 **
 ┌┬─────────────────⦿
 │├─────────────────╮
-│├ ᴛɢ ɴᴀᴍᴇ - ʟᴇɢᴇɴᴅ ᴍɪᴄᴋᴇʏ
-│├ ʀᴇᴀʟ ɴᴀᴍᴇ - ᴛᴏsᴜ ᴍᴇᴍᴏɴ
+│├ ᴛɢ ɴᴀᴍᴇ - MAANAV
+│├ ʀᴇᴀʟ ɴᴀᴍᴇ - MAANAV
 │├─────────────────╯
 ├┼─────────────────⦿
-├┤~ @THE_INCRICIBLE
-├┤~ @LEGEND_MICKEY
-├┤~ @ABOUT_GODFATHER
+├┤~ @maanavbots
+├┤~ @ABOUT_MAANAV
+├┤~ @aboutkanha
 ├┼─────────────────⦿
 │├─────────────────╮
-│├OWNER│ @LEGEND_MICKEY
+│├OWNER│ @oyemaanav
 │├─────────────────╯
 └┴─────────────────⦿
 **
@@ -32,24 +32,24 @@ start_txt = """
 async def start(_, msg):
     buttons = [
         [ 
-          InlineKeyboardButton(" 𝗟𝗘𝗚𝗘𝗡𝗗 𝗠𝗜𝗖𝗞𝗘𝗬", url=f"https://t.me/LEGEND_MICKEY")
+          InlineKeyboardButton(" 𝗠𝗔𝗔𝗡𝗔𝗩", url=f"https://t.me/ABOUT_MAANAV")
         ],
         [
-          InlineKeyboardButton("ʜᴇʟᴘ", url="https://t.me/THE_INCRICIBLE"),
-          InlineKeyboardButton("ʀᴇᴘᴏ", url="https://t.me/THE_INCRICIBLE"),
+          InlineKeyboardButton("ʜᴇʟᴘ", url="https://t.me/ABOUT_MAANAV"),
+          InlineKeyboardButton("ʀᴇᴘᴏ", url="https://t.me/ABOUT_MAANAV"),
           ],
                [
-                InlineKeyboardButton("ɪɴᴄʀɪᴄɪʙʟᴇ ɴᴇᴛᴡᴏʀᴋ", url=f"https://t.me/THE_INCRICIBLE"),
+                InlineKeyboardButton("ɪɴᴄʀɪᴄɪʙʟᴇ ɴᴇᴛᴡᴏʀᴋ", url=f"https://t.me/ABOUT_MAANAV"),
 ],
 [
-InlineKeyboardButton("ᴏғғɪᴄɪᴀʟ ʙᴏᴛ", url=f"https://t.me/ZOYUMUSICBOT"),
+InlineKeyboardButton("ᴏғғɪᴄɪᴀʟ ʙᴏᴛ", url=f"https://t.me/maanavXmuziccbot"),
 
         ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
     await msg.reply_photo(
-        photo="https://files.catbox.moe/0wtv2m.jpg",
+        photo="https://files.catbox.moe/pt0ytt.jpg",
         caption=start_txt,
         reply_markup=reply_markup
     )
