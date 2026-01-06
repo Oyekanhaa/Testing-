bad001a7262f8a887ab46cf3be7e999876daf794
𝐊𝐚𝐧𝐡𝐚 <ayush03kmr@gmail.com>
2026-01-06 09:52:41 +0530

    Update play.py

--- SACHIN_MUSIC/utils/inline/play.py
+++ SACHIN_MUSIC/utils/inline/play.py
@@ -1,10 +1,9 @@
 import math
-from config import SUPPORT_CHAT, OWNER_USERNAME
-from pyrogram.types import InlineKeyboardButton
-from SACHIN_MUSIC import app
-import config
-from SACHIN_MUSIC.utils.formatters import time_to_seconds
 
+from pyrogram.types import InlineKeyboardButton
+
+from SACHIN_MUSIC.utils.formatters import time_to_seconds
+from SACHIN_MUSIC import app
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
@@ -34,40 +33,43 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "♡╌╌╌╌╌╌╌╌"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "╌♡╌╌╌╌╌╌╌"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "╌╌♡╌╌╌╌╌╌"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "╌╌╌♡╌╌╌╌╌"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "╌╌╌╌♡╌╌╌╌"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "╌╌╌╌╌♡╌╌╌"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "╌╌╌╌╌╌♡╌╌"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "╌╌╌╌╌╌╌♡╌"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "╌╌╌╌╌╌╌╌♡╌"
     else:
-        bar = "—————————◉"
+        bar = "╌╌╌╌╌╌╌╌╌♡"
     buttons = [
-                [
+        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-		[
-         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url=f"{SUPPORT_CHAT}",),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
+            InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ[✚]", url=f"https://t.me/{app.username}?startgroup=true"),
+            InlineKeyboardButton(text="ᴄʟᴏsᴇ[✗]", callback_data="close")
+         ]
     ]
     return buttons
 
@@ -75,8 +77,11 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", user_id=config.OWNER_ID),
-         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url=f"{SUPPORT_CHAT}",),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
@@ -88,11 +93,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"SACHINPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"SagarPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"SACHINPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"SagarPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
